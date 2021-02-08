6d201984a8532fcb1f0c41515e22555dc44aa6e4
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-08 15:36:30 +0530

    Login page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Views/Home";
+import Login from "./Views/User/Login";
+import Signup from "./Views/User/Signup";
+import Dashboard from "./Views/User/Dashboard";
+import LoginHealthcare from "./Views/Healthcare/LoginHealthcare";
+import SignupHealthcare from "./Views/Healthcare/SignupHealthcare";
+import DashboardHealthcare from "./Views/Healthcare/DashboardHealthcare";
 
 function App() {
   return (
     <div className="App">
-      Hello there
-      <Navbar />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* User routes */}
+          <Route path="/user/login" component={Login} />
+          <Route path="/user/signup" component={Signup} />
+          <Route path="/user/:slug" component={Dashboard} />
+          {/* Healthcare worker routes */}
+          <Route path="/healthcare/login" component={Login} />
+          <Route path="/healthcare/signup" component={SignupHealthcare} />
+          <Route path="/healthcare/:slug" component={DashboardHealthcare} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Views/User/Login.js
+++ src/Views/User/Login.js
@@ -1,5 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import image from "../../Assets/loginuser.png";
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row min-vh-100 w-100">
+        <div className="justify-content-center align-center col-lg-5 bg-light col-sm-12  text-white d-flex flex-col">
+          <div className="text-center container align-self-center ">
+            <img src={image} className="img-fluid w-75 h-75" />
+            <h1 className=" text-dark">ASAP</h1>
+            <p className="text-dark">Healthcare anytime,anywhere!</p>
+          </div>
+        </div>
+        <div className="col-lg-7 col-sm-12 d-flex flex-col">
+          <div className="container align-self-center">
+            <h2>Login</h2>
+            <p>
+              New here? Consider
+              <Link to="/user/signup"> SignUp</Link>
+            </p>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input type="email" className="form-control" id="email" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input type="password" className="form-control" id="password" />
+              </div>
+
+              <button type="submit" className="btn  btn-primary btn-block">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
