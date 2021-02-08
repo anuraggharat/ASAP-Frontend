dc936778ed7c788814a6df334063d978939f4446
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-08 17:25:37 +0530

    user dashboard done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,49 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="#">
+              ASAP
+            </a>
+            <div class="dropdown ml-auto">
+              <button
+                class="btn btn-primary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                anuraggharat55@gmail.com
+              </button>
+              <ul
+                class="dropdown-menu w-100"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
--- src/Views/User/Dashboard.js
+++ src/Views/User/Dashboard.js
@@ -1,5 +1,46 @@
 import React from "react";
+import Navbar from "../../Components/Navbar";
 
 export default function Dashboard() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="container mt-5">
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-lg btn-danger shadow-lg danger-button">
+            <i class="bi bi-exclamation-circle "></i>
+            EMERGENCY
+          </button>
+        </div>
+        <div className="d-flex mt-5 text-muted">
+          <div class="card w-100 shadow-lg border-0">
+            <div class="card-body">
+              <div className="row">
+                <div className="col-6 border-bottom">
+                  <h5>Name</h5>
+                  <p>Anurag Gharat</p>
+                </div>
+                <div className="col-6  border-bottom">
+                  <h5>Email</h5>
+                  <p>anuraggharat55@gmail.com</p>
+                </div>
+                <div className="col-6  border-bottom mt-3">
+                  <h5>Phone number</h5>
+                  <p>7745050822</p>
+                </div>
+                <div className="col-6  border-bottom mt-3">
+                  <h5>Date of Birth</h5>
+                  <p>22 May 2017</p>
+                </div>
+                <div className="col-6 mt-3">
+                  <h5>Gender</h5>
+                  <p>Male</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
