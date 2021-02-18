8825b02b0af9c4b1988b9148b25a88fb054c8b6f
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-18 23:52:55 +0530

    user login and signup values done

--- src/Views/Healthcare/LoginHealthcare.js
+++ src/Views/Healthcare/LoginHealthcare.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import image from "../../Assets/loginhealthcare.svg";
 import MinorComponent from "../../Components/MinorComponent";
 
 export default function LoginHealthcare() {
+  const [account, setAccount] = useState();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { email, password } = values;
+
+  //handleChange function to set input values
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  //value submission function
+  const submitValues = (e) => {
+    e.preventDefault();
+    console.log("Submittted values", values);
+  };
   return (
     <>
       <div className="row min-vh-100 w-100">
         <MinorComponent image={image} />
-        <div className="col-lg-7 col-sm-12 d-flex justify-content-center flex-column">
+        <div className="col-lg-7 col-sm-12 d-flex justify-content-center flex-column bg-light">
           <div className="container">
             <h1>
               Login{" "}
@@ -18,18 +37,32 @@ export default function LoginHealthcare() {
               New here? Consider
               <Link to="/user/signup"> SignUp</Link>
             </p>
-            <form>
+            <form onSubmit={(e) => submitValues(e)}>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">
                   Email address
                 </label>
-                <input type="email" className="form-control" id="email" />
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">
                   Password
                 </label>
-                <input type="password" className="form-control" id="password" />
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
 
               <button type="submit" className="btn  btn-primary btn-block">
--- src/Views/User/Signup.js
+++ src/Views/User/Signup.js
@@ -1,9 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import image from "../../Assets/signupuser.svg";
 import MinorComponent from "../../Components/MinorComponent";
 
 export default function Signup() {
+  const [values, setValues] = useState({
+    name: "",
+    number: "",
+    gender: "",
+    age: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { email, password, name, number, gender, dob } = values;
+
+  //handleChange function to set input values
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log("====================================");
+  console.log(values);
+  console.log("====================================");
+  //value submission function
+  const submitValues = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    console.log("Submittted values", values);
+  };
   return (
     <>
       <div className="row min-vh-100 w-100">
@@ -15,24 +41,45 @@ export default function Signup() {
               Already registered? Consider
               <Link to="/user/login"> Login</Link>
             </p>
-            <form>
+            <form onSubmit={(e) => submitValues(e)}>
               <div className="mb-3 mt-3">
                 <label htmlFor="name" className="form-label">
                   Your Name
                 </label>
-                <input type="name" className="form-control" id="name" />
+                <input
+                  type="name"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">
                   Email address
                 </label>
-                <input type="email" className="form-control" id="email" />
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
               <div className="mb-3">
                 <label htmlFor="number" className="form-label">
                   Your Phone number
                 </label>
-                <input type="text" className="form-control" id="number  " />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="number  "
+                  name="number"
+                  value={number}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
               <div className="mb-3 row">
                 <div className="col-6">
@@ -44,17 +91,24 @@ export default function Signup() {
                     className="form-control"
                     id="dob"
                     placeholder="In 27 May 1990 format"
+                    name="dob"
+                    value={dob}
+                    onChange={(e) => handleChange(e)}
                   />
                 </div>
                 <div className="col-6">
                   <label htmlFor="gender" className="form-label">
                     Gender
                   </label>
-                  <select className="form-control">
-                    <option defaultValue="Male">Select one</option>
-                    <option value="1">Male</option>
-                    <option value="2">Female</option>
-                    <option value="3">Other</option>
+                  <select
+                    className="form-control"
+                    onChange={(e) => handleChange(e)}
+                    name="gender"
+                  >
+                    <option defaultValue={gender}>Select one</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
                   </select>
                 </div>
               </div>
@@ -62,7 +116,14 @@ export default function Signup() {
                 <label htmlFor="password" className="form-label">
                   Password
                 </label>
-                <input type="password" className="form-control" id="password" />
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => handleChange(e)}
+                />
               </div>
 
               <button type="submit" className="btn  btn-primary btn-block">
