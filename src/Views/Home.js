c25986fc7921994b8420700890ba032cd5a7dbab
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-19 00:50:51 +0530

    home page done

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,13 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      <Link to="/user/home">Home</Link>
-      <Link to="/user/login">user login</Link>
-      <Link to="/user/signup">user signup</Link>
-      <Link to="/healthcare/login">healthcare login</Link>
-      <Link to="/healthcare/signup">healthcare signup</Link>
-      <Link to="/healthcare/home">healthcare home</Link>
+    <div className="w-100 min-vh-100 bg-home d-flex flex-column justify-content-center text-center text-white">
+      <div className="overlay"></div>
+      <div className="container z-10">
+        <h1 className=" display-3">Healthcare Anywhere, Anytime!</h1>
+        <div className="w-75 mx-auto px-5 my-4">
+          <p>
+            A online portal for users to request healthcare help. A nearest
+            healthcare station is alloted according to users position.
+          </p>
+        </div>
+        <div className="d-flex justify-content-around w-50 mx-auto">
+          <Link to="/user/login" className="btn btn-primary">
+            User Login
+          </Link>
+          <Link to="/healthcare/login" className="btn btn-primary">
+            Healthcare Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
